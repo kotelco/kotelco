7bec13cda411142848e7409246b80294ef5e8174
Kotelco <kotelco@ukr.net>
2019-06-15 14:49:32 +0300

    table_example

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,8 @@
 var scrol = document.querySelectorAll(".scro");
 var wrap = document.getElementById("wrapper");
-var mobnav = document.getElementById("mobile_nav");
-var hamb = document.getElementById("menuToggle");
 var mous = document.getElementById("mouse");
 var herotext = document.querySelectorAll(".hero_text");
 var herologo = document.getElementById("hero_logo");
-
 function wscroll() {
     if (wrap.scrollTop >= 200) {
         scrol.forEach(el => el.classList.add('scrol'));
@@ -20,20 +17,4 @@ function wscroll() {
         herologo.style.opacity = '0';
     }
 }
-
 wrap.onscroll = wscroll;
-
-
-function mobmenu_check()  {
-    if (mobnav.style.transform == 'none') {
-        mobnav.style.transform = 'translate(-100%, 0)';
-    wrap.style.opacity = '1';
-
-    }
-    else {
-        wrap.style.opacity = '.1';
-        mobnav.style.transform = 'none';
-    }
-}
-
-hamb.onclick = mobmenu_check;
