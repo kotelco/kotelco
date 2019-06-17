852c11d1064981584c4d893ffb9eef3ee60caa62
Kotelco <kotelco@ukr.net>
2019-06-17 17:03:33 +0300

    tables_aogv

--- js/not_used/custom.js
+++ js/not_used/custom.js
@@ -3,6 +3,9 @@ var wrap = document.getElementById("wrapper");
 var mous = document.getElementById("mouse");
 var herotext = document.querySelectorAll(".hero_text");
 var herologo = document.getElementById("hero_logo");
+var mobnav = document.getElementById("mobile_nav");
+var hamb = document.getElementById("menuToggle");
+var wrap_def = document.getElementById("default_layout");
 function wscroll() {
     if (wrap.scrollTop >= 200) {
         scrol.forEach(el => el.classList.add('scrol'));
@@ -18,3 +21,15 @@ function wscroll() {
     }
 }
 wrap.onscroll = wscroll;
+
+function mobmenu_check()  {
+    if (mobnav.style.transform == 'none') {
+        mobnav.style.transform = 'translate(-100%, 0)';
+        wrap_def.style.opacity = '1';
+    }
+    else {
+        wrap_def.style.opacity = '.1';
+        mobnav.style.transform = 'none';
+    }
+}
+hamb.onclick = mobmenu_check;
